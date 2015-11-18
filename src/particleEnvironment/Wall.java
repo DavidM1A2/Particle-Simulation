aef91ffad8cfd6121a05685def8925b088265688
Wendy Wang <wnd.w.wng@gmail.com>
2015-11-18 13:23:22 -0700

    minor formatting (commit to sync)

--- src/particleEnvironment/Wall.java
+++ src/particleEnvironment/Wall.java
@@ -5,79 +5,58 @@ import java.util.List;
 import processing.core.PApplet;
 import processing.core.PVector;
 
-public class Wall extends SceneObject
-{
+public class Wall extends SceneObject {
 	private final PApplet parent;
 	private final PVector location;
 	private final PVector size;
 
-	public Wall(PVector location, PApplet parent, PVector size)
-	{
+	public Wall(PVector location, PApplet parent, PVector size) {
 		this.location = location;
 		this.parent = parent;
 		this.size = size;
 	}
 
 	@Override
-	public void display()
-	{
+	public void display() {
 		this.parent.rect(this.location.x, this.location.y, this.size.x, this.size.y);
 	}
 
 	@Override
-	public void update(List<SceneObject> sceneObjects)
-	{
-		for (SceneObject sceneObject : sceneObjects)
-		{
-			if (sceneObject instanceof Particle)
-			{
+	public void update(List<SceneObject> sceneObjects) {
+		for (SceneObject sceneObject : sceneObjects) {
+			if (sceneObject instanceof Particle) {
 				Particle particle = (Particle) sceneObject;
-				
 				// colliding with right
-				 if (((particle.getParticleLocation().x +
-				 particle.getParticleRadius()) > this.location.x) &&
-				 ((particle.getParticleLocation().x +
-				 particle.getParticleRadius()) < (this.location.x +
-				 this.size.x)))
-				 {
-					 if (((particle.getParticleLocation().y + 
-					 particle.getParticleRadius()) >
-					 this.location.y) &&((particle.getParticleLocation().y + 
-					 particle.getParticleRadius()) <
-					 (this.location.y +this.size.y)))
-					 {
-						 particle.invertXVelocity();
-				     }
-				 }
-				 // colliding with top
-				 if (((particle.getParticleLocation().y +
-				 particle.getParticleRadius()) > this.location.y) &&
-				 ((particle.getParticleLocation().y +
-				 particle.getParticleRadius()) < (this.location.y +
-				 this.size.y)))
-				 {
-					 if (((particle.getParticleLocation().x + 
-					 particle.getParticleRadius()) >
-					 this.location.x) && ((particle.getParticleLocation().x + 
-					 particle.getParticleRadius()) <
-					 (this.location.x +this.size.x)))
-					 {
-						 particle.invertYVelocity();
-				     }
-				 }
+				if (((particle.getParticleLocation().x + particle.getParticleRadius()) > this.location.x)
+						&& ((particle.getParticleLocation().x + particle.getParticleRadius()) < (this.location.x
+						+ this.size.x))) {
+				if (((particle.getParticleLocation().y + particle.getParticleRadius()) > this.location.y)
+						&& ((particle.getParticleLocation().y + particle.getParticleRadius()) < (this.location.y
+						+ this.size.y))) {
+						particle.invertXVelocity();
+					}
+				}
+				// colliding with top
+				if (((particle.getParticleLocation().y + particle.getParticleRadius()) > this.location.y)
+						&& ((particle.getParticleLocation().y + particle.getParticleRadius()) < (this.location.y
+						+ this.size.y))) {
+				if (((particle.getParticleLocation().x + particle.getParticleRadius()) > this.location.x)
+						&& ((particle.getParticleLocation().x + particle.getParticleRadius()) < (this.location.x
+						+ this.size.x))) {
+						particle.invertYVelocity();
+					}
+				}
 			}
 		}
 	}
 
 	@Override
-	public boolean isDead()
-	{
+	public boolean isDead() {
 		return false;
 	}
 
 	@Override
-	public int renderPriority()
-	{
+	public int renderPriority() {
 		return 3;
 	}
 
